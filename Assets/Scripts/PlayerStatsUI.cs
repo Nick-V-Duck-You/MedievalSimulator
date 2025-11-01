55868ba07e1d424b4fd4dba785ca3040dcb431c6
Nick-V-Duck-You <valentinedead320@gmail.com>
2025-11-01 12:38:50 +0300

    UI for player stats added

--- Assets/Scripts/PlayerStatsUI.cs
+++ Assets/Scripts/PlayerStatsUI.cs
@@ -4,17 +4,24 @@ using UnityEngine.UI;
 public class PlayerStatsUI : MonoBehaviour
 {
 	public Image healthBar;
+	public Image hungerBar;
+	public Image inebriationBar;
+	
 	public PlayerStats player;
-    public GameObject GPlayer;
 
 	void Start()
     {
-        GPlayer = GameObject.FindGameObjectWithTag("Player");
-        GPlayer.GetComponent<PlayerStats>().ChangeStatValue("HP",-50);
-    }
+		healthBar = healthBar.GetComponent<Image>();
+		hungerBar = hungerBar.GetComponent<Image>();
+		inebriationBar = inebriationBar.GetComponent<Image>();
+		player = FindFirstObjectByType<PlayerStats>();
+	}
 
     void Update()
     {
-        
-    }
+		healthBar.fillAmount = player.HP / player.maxHP;
+		hungerBar.fillAmount = player.hunger / player.maxHunger;
+		inebriationBar.fillAmount = player.inebriation / player.maxInebriation;
+
+	}
 }

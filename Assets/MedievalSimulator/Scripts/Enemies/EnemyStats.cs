047e1234094377565ce459a9ab4735147d67c792
Nick-V-Duck-You <valentinedead320@gmail.com>
2025-12-10 14:45:28 +0300

    added enemy controller

---
+++ Assets/MedievalSimulator/Scripts/Enemies/EnemyStats.cs
@@ -0,0 +1,10 @@
+using NUnit.Framework;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour
+{
+    public int hp;
+    public float speed;
+    public float damage;
+    public Transform[] waypoints;
+}
